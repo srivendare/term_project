af272fd8a621079f4ed61dcd175a7e1583293bb9
gulinigeer <aihemaiti.gu@northeastern.edu>
2022-12-01 23:17:57 +0800

    analyse

--- term_project/term_project/src/ui/analyse/Chart.java
+++ term_project/term_project/src/ui/analyse/Chart.java
@@ -183,24 +183,24 @@ public ChartPanel getSalesVolumeChart(){
   
               DefaultPieDataset data = getDataset();
 	      JFreeChart chart = ChartFactory.createPieChart3D("Percentage of sales",data,true,false,false);
-	    //设置百分比
+	    
 	      PiePlot pieplot = (PiePlot) chart.getPlot();
-	      DecimalFormat df = new DecimalFormat("0.00%");//获得一个DecimalFormat对象，主要是设置小数问题
-	      NumberFormat nf = NumberFormat.getNumberInstance();//获得一个NumberFormat对象
+	      DecimalFormat df = new DecimalFormat("0.00%");
+	      NumberFormat nf = NumberFormat.getNumberInstance();
 	      StandardPieSectionLabelGenerator sp1 = new StandardPieSectionLabelGenerator("{0}  {2}", nf, df);//获得StandardPieSectionLabelGenerator对象
-	      pieplot.setLabelGenerator(sp1);//设置饼图显示百分比
+	      pieplot.setLabelGenerator(sp1);
 	  
-	  //没有数据的时候显示的内容
-	      pieplot.setNoDataMessage("无数据显示");
+	  
+	      pieplot.setNoDataMessage("No Data");
 	      pieplot.setCircular(false);
 	      pieplot.setLabelGap(0.02D);
 	  
-	      pieplot.setIgnoreNullValues(true);//设置不显示空值
-	      pieplot.setIgnoreZeroValues(true);//设置不显示负值
+	      pieplot.setIgnoreNullValues(true);
+	      pieplot.setIgnoreZeroValues(true);
 	     ChartPanel frame1=new ChartPanel (chart,true);
-	      chart.getTitle().setFont(new Font("宋体",Font.BOLD,20));//设置标题字体
-	      PiePlot piePlot= (PiePlot) chart.getPlot();//获取图表区域对象
-	      piePlot.setLabelFont(new Font("宋体",Font.BOLD,10));//解决乱码
+	      chart.getTitle().setFont(new Font("宋体",Font.BOLD,20));
+	      PiePlot piePlot= (PiePlot) chart.getPlot();
+	      piePlot.setLabelFont(new Font("宋体",Font.BOLD,10));
 	      chart.getLegend().setItemFont(new Font("黑体",Font.BOLD,10));
               return frame1;
 
