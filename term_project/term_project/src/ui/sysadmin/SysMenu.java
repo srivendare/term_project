90f9bc2eb1f5119b7c34b8c96bf66727347fa583
Rui SHANG <shangrui0609@gmail.com>
2022-12-07 02:03:56 +0800

    update admin panel

--- term_project/term_project/src/ui/sysadmin/SysMenu.java
+++ term_project/term_project/src/ui/sysadmin/SysMenu.java
@@ -46,8 +46,13 @@ public class SysMenu extends javax.swing.JFrame {
         splitPane = new javax.swing.JSplitPane();
         jPanel1 = new javax.swing.JPanel();
         btnUser = new javax.swing.JButton();
-        btnSupplier1 = new javax.swing.JButton();
+        btnSup = new javax.swing.JButton();
         btnLogin = new javax.swing.JButton();
+        btnData = new javax.swing.JButton();
+        btnSou2 = new javax.swing.JButton();
+        btnFin1 = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -65,14 +70,14 @@ public class SysMenu extends javax.swing.JFrame {
             }
         });
 
-        btnSupplier1.setBackground(new java.awt.Color(255, 255, 255));
-        btnSupplier1.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
-        btnSupplier1.setText("Supplier");
-        btnSupplier1.setBorderPainted(false);
-        btnSupplier1.setContentAreaFilled(false);
-        btnSupplier1.addActionListener(new java.awt.event.ActionListener() {
+        btnSup.setBackground(new java.awt.Color(255, 255, 255));
+        btnSup.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
+        btnSup.setText("Supply");
+        btnSup.setBorderPainted(false);
+        btnSup.setContentAreaFilled(false);
+        btnSup.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnSupplier1ActionPerformed(evt);
+                btnSupActionPerformed(evt);
             }
         });
 
@@ -89,45 +94,105 @@ public class SysMenu extends javax.swing.JFrame {
             }
         });
 
+        btnData.setBackground(new java.awt.Color(255, 255, 255));
+        btnData.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
+        btnData.setText("Data");
+        btnData.setBorderPainted(false);
+        btnData.setContentAreaFilled(false);
+        btnData.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDataActionPerformed(evt);
+            }
+        });
+
+        btnSou2.setBackground(new java.awt.Color(255, 255, 255));
+        btnSou2.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
+        btnSou2.setText("Sourcing");
+        btnSou2.setBorderPainted(false);
+        btnSou2.setContentAreaFilled(false);
+        btnSou2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSou2ActionPerformed(evt);
+            }
+        });
+
+        btnFin1.setBackground(new java.awt.Color(255, 255, 255));
+        btnFin1.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
+        btnFin1.setText("Finance");
+        btnFin1.setBorderPainted(false);
+        btnFin1.setContentAreaFilled(false);
+        btnFin1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnFin1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(btnUser, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 12, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnSup, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnSou2, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnData, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnFin1, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btnSupplier1, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap()))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnUser, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(148, 148, 148)
+                .addGap(117, 117, 117)
+                .addComponent(btnSup, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnSou2, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(12, 12, 12)
+                .addComponent(btnFin1, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnData, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(btnUser, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 399, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 168, Short.MAX_VALUE)
                 .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(15, 15, 15))
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel1Layout.createSequentialGroup()
-                    .addGap(222, 222, 222)
-                    .addComponent(btnSupplier1, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(373, Short.MAX_VALUE)))
         );
 
         splitPane.setLeftComponent(jPanel1);
+
+        jLabel1.setFont(new java.awt.Font("Lucida Grande", 0, 30)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel1.setText("Please Select a Module");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(194, 194, 194)
+                .addComponent(jLabel1)
+                .addContainerGap(237, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(244, 244, 244)
+                .addComponent(jLabel1)
+                .addContainerGap(367, Short.MAX_VALUE))
+        );
+
+        splitPane.setRightComponent(jPanel2);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(splitPane, javax.swing.GroupLayout.DEFAULT_SIZE, 946, Short.MAX_VALUE)
+            .addComponent(splitPane, javax.swing.GroupLayout.DEFAULT_SIZE, 927, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -143,9 +208,9 @@ public class SysMenu extends javax.swing.JFrame {
         splitPane.setRightComponent(manageUser.getEditPane());
     }//GEN-LAST:event_btnUserActionPerformed
 
-    private void btnSupplier1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSupplier1ActionPerformed
+    private void btnSupActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSupActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_btnSupplier1ActionPerformed
+    }//GEN-LAST:event_btnSupActionPerformed
 
     private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
         // TODO add your handling code here:
@@ -153,6 +218,21 @@ public class SysMenu extends javax.swing.JFrame {
         new Login_Form().setVisible(true);
 
     }//GEN-LAST:event_btnLoginActionPerformed
+
+    private void btnDataActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDataActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnDataActionPerformed
+
+    private void btnSou2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSou2ActionPerformed
+        // TODO add your handling code here:
+        AdminSou adminSou = new AdminSou();
+        splitPane.setRightComponent(adminSou);
+             
+    }//GEN-LAST:event_btnSou2ActionPerformed
+
+    private void btnFin1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnFin1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnFin1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -205,10 +285,15 @@ public class SysMenu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnData;
+    private javax.swing.JButton btnFin1;
     private javax.swing.JButton btnLogin;
-    private javax.swing.JButton btnSupplier1;
+    private javax.swing.JButton btnSou2;
+    private javax.swing.JButton btnSup;
     private javax.swing.JButton btnUser;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JSplitPane splitPane;
     // End of variables declaration//GEN-END:variables
 }
