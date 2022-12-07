8e2f50cb842605706a1631bf856bbfa938cb01dd
Qiaotong Huang <huang.qiaot@northeastern.edu>
2022-12-07 23:29:53 +0800

    finance

--- term_project/term_project/src/ui/finance/TaxOffice.java
+++ term_project/term_project/src/ui/finance/TaxOffice.java
@@ -55,11 +55,7 @@ public class TaxOffice extends javax.swing.JFrame {
         jTable_Tax.setModel(mmodel);
         jTable_Tax.setRowHeight(45);
     }
-    
-    
-    
-    
-    
+   
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -246,7 +242,6 @@ public class TaxOffice extends javax.swing.JFrame {
         
     }//GEN-LAST:event_jTable_TaxMouseClicked
 
-    
     // button add a new tax
     private void jButton_INSERT_ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_INSERT_ActionPerformed
 
@@ -262,7 +257,6 @@ public class TaxOffice extends javax.swing.JFrame {
         
     }//GEN-LAST:event_jButton_INSERT_ActionPerformed
 
-    
     // button update tax
     private void jButton_UPDATE_ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_UPDATE_ActionPerformed
 
@@ -313,8 +307,6 @@ public class TaxOffice extends javax.swing.JFrame {
            
     }//GEN-LAST:event_jButton_CLEAR_ActionPerformed
 
-
-    
     // function to check empty fields
     public boolean verifFields()
     {
@@ -325,58 +317,6 @@ public class TaxOffice extends javax.swing.JFrame {
         }else{
             return true;
         }
-    }
-    
-    
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TaxOffice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TaxOffice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TaxOffice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TaxOffice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new TaxOffice().setVisible(true);
-            }
-        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
